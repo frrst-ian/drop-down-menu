accdd3cf7ffdac706c5dbc8a94e1507c03e89504
frrst-ian <frrstxd@proton.me>
2024-07-26 18:22:49 +0800

    Add dropdown menu
    Add a simple dropdown menu so , shows up when hovered

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# project-template
+# Simple Drop Down Menu
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './styles.css';
+import menuIcon from './assets/images/menu.svg';
+console.log('menuIcon:', menuIcon)
+const button = document.getElementById('dropdown-button');
+
+const img = new Image();
+img.src = menuIcon;
+img.alt = 'menu';
+img.width = 40;
+img.height = 40;
+console.log('Image element:', img);
+
+button.appendChild(img);
+
+const dropDownDiv = document.querySelector('.dropdown-content');
+dropDownDiv.classList.add('visible');
+
+
+
